2d62e8623b001d0f9aec94953158f131646a076e
DianaEnoiu <dianaioanaenoiu@gmail.com>
2022-08-17 19:55:42 +0300

    retrigger

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Love George
+          Ily
         </p>
         <a
           className="App-link"
